51bc24636bc6039c32153b1e1a764df004695c42
Druks <dariusvolkmann@gmail.com>
2022-10-03 17:00:49 -0300

    first commit

---
+++ src/components/screens/Home.js
@@ -0,0 +1,12 @@
+import Header from "../../components/Header";
+
+const Home = () => {
+    return (
+        <>
+            <h1>Home</h1>
+            <Header/>
+        </>
+    )
+}
+
+export default Home
